7480e1a609f355048d5e126f72e7f51d168e15ab
Marshall Turner <turnerm5@gmail.com>
2014-10-08 11:30:25 -0700

    Kirby-fy the awards list

--- site/templates/about.php
+++ site/templates/about.php
@@ -117,47 +117,9 @@
                     <div class="title-devider"></div>
                 </div>
 
-                <div id="Container">
-                    
                     <div class="row">
-                        <div class="col-md-10 col-md-offset-1">
-                            <p class="sort_list">
-                                Sort by: 
-                                <a class="sort" data-sort="date:asc">Date</a>
-                                <a class="sort" data-sort="project:asc">Project</a>
-                            </p>
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="col-md-10 col-md-offset-1">
-                            <div class="mix" data-project="2" data-date="1"><p>AIA WA Council Civic Design Awards, Citation, 2013 | Fire Station 30, City of Seattle</p></div>
-                            <div class="mix" data-project="5" data-date="2"><p>AIA Washington Council Civic Design Awards, Honor Award, 2012 | Opportunity Center, North Seattle College</p></div>
-                            <div class="mix" data-project="2" data-date="3"><p>Copper Development Association, Copper In Architecture Award, 2012 | Fire Station 30, City of Seattle</p></div>
-                            <div class="mix" data-project="2" data-date="4"><p>AIA Project Of The Month, January 2012 | Fire Station 30, City of Seattle</p></div>
-                            <div class="mix" data-project="6" data-date="5"><p>Masonry Institute of Washington, Merit Award 2012 | Maier Hall, Peninsula College</p></div>
-                            <div class="mix" data-project="2" data-date="6"><p>Seattle Design Commission, Award For Design Excellence, 2011 | Fire Station 30, City of Seattle</p></div>
-                            <div class="mix" data-project="2" data-date="7"><p>F.I.E.R.O. Honor Award, 2011 | Fire Station 30, City of Seattle</p></div>
-                            <div class="mix" data-project="6" data-date="8"><p>AIA Seattle Honor Awards, Commendation, 2011 | Maier Hall, Peninsula College</p></div>
-                            <div class="mix" data-project="6" data-date="9"><p>American School & University Design Awards, Post-Secondary Citation, 2011 | Maier Hall, Peninsula College</p></div>
-                            <div class="mix" data-project="6" data-date="10"><p>AIA Washington Council Civic Design Awards, Honor Award, 2010 | Library Media Center, Peninsula College</p></div>
-                            <div class="mix" data-project="7" data-date="11"><p>American School & University Outstanding Design: Renovation/Modernization, 2010 | Culinary Academy, Seattle Central College</p></div>
-                            <div class="mix" data-project="6" data-date="12"><p>American School & University Design Awards, Louis I. Kahn Citation, 2009 | Library Media Center, Peninsula College</p></div>
-                            <div class="mix" data-project="8" data-date="13"><p>AIA Pacific NW Region Awards, Merit Award, 2009 | Douglass-Truth Branch Library, The Seattle Public Library</p></div>
-                            <div class="mix" data-project="8" data-date="14"><p>Copper Development Association, Copper In Architecture Award, 2009 | Douglass-Truth Branch Library, The Seattle Public Library</p></div>
-                            <div class="mix" data-project="8" data-date="15"><p>AIA Washington Council Civic Design Awards, Honor Award, 2007 | Douglass-Truth Branch Library, The Seattle Public Library</p></div>
-                            <div class="mix" data-project="8" data-date="16"><p>AIA Project Of The Month, February 2007 | Douglass-Truth Branch Library, The Seattle Public Library</p></div>
-                            <div class="mix" data-project="8" data-date="17"><p>Seattle Magazine, Best of Architecture Awards, 2006 | Douglass-Truth Branch Library, The Seattle Public Library</p></div>
-                            <div class="mix" data-project="4" data-date="18"><p>AIA Washington Council Civic Design Awards, Merit Award, 2004 | Spellman Library, Grays Harbor College</p></div>
-                            <div class="mix" data-project="9" data-date="19"><p>AIA Washington Council Civic Design Awards, Citation, 2003 | Art Gallery, Tacoma Community College</p></div>
-                            <div class="mix" data-project="10" data-date="20"><p>AIA Pacific NW Regional Award, Merit Award, 2000 | Temple B'nai Torah</p></div>
-                            <div class="mix" data-project="10" data-date="21"><p>Masonry Institute Most Artistic Use of Masonry Award, 1998 | Temple B'nai Torah</p></div>
-                            <div class="mix" data-project="10" data-date="22"><p>AIA Seattle Honor Awards, Merit Award, 1998 | Temple B'nai Torah</p></div>
-                            <div class="mix" data-project="10" data-date="23"><p>Masonry Institute Honor Award, 1998 | Temple B'nai Torah</p></div>
-                            <div class="mix" data-project="3" data-date="24"><p>AIA Project of the Month, June 1997 | Student Activities Offices, The Evergreen State College</p></div>
-                            <div class="mix" data-project="3" data-date="25"><p>AIA Seattle Honor Awards, Merit Award, 1995 | Student Activities Offices, The Evergreen State College</p></div>
-                            <div class="mix" data-project="1" data-date="26"><p>AIA Seattle Honor Awards, Merit Award, 1993 | College Services Building, Bellingham Technical College</p></div>
-                            <div class="mix" data-project="3" data-date="27"><p>Bonneville Power Administration/Portland Chapter AIA, Honor Award, 1993 | Student Activities Offices, The Evergreen State College</p></div>
+                        <div class="col-md-10 col-md-offset-1 text-center">
+                            <p><?php echo kirbytext($page->awards()) ?></p>
                         </div>
                     </div> <!-- End .row -->
                 </div> <!-- End #Container -->
