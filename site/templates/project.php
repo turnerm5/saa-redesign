e967ee71d7ef1a2ce390d4b9080e30dc2eaff086
Marshall Turner <turnerm5@gmail.com>
2014-10-08 09:49:40 -0700

    back to two columns for project text

--- site/templates/project.php
+++ site/templates/project.php
@@ -32,7 +32,7 @@
                                         <?php if($page->hasOneImage()): ?>
                                             <div class="post-box col-md-12 col-lg-12 col-sm-12 col-xs-12">
                                         <?php else: ?>
-                                            <div class="post-box col-md-4 col-lg-4 col-sm-4 col-xs-3">
+                                            <div class="post-box col-md-6 col-lg-6 col-sm-6 col-xs-3">
                                         <?php endif ?>
                                             <li>
                                                 <a href="<?php echo $image->url() ?>" data-lightbox="project_photos" data-title="<?php echo $image->caption() ?>">
